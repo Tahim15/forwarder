c2e7b3e551659fb5a7b07dd47bfa20e9423ab9e9
♕︎ Tʜᴇ Wʜɪᴛᴇ Dᴇᴠɪʟ ♕︎ <99538849+Tahim15@users.noreply.github.com>
2025-03-01 09:36:42 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+import re
 from config import API_ID, API_HASH, SESSION_NAME, SOURCE_CHANNEL, DEST_CHANNEL, TG_WORKERS
 
 app = Client(
@@ -10,18 +11,16 @@ app = Client(
 
 @app.on_message(filters.chat(SOURCE_CHANNEL) & filters.text)
 async def forward_message(client, message):
-    gofile_link = None
-    streamtape_link = None
+    # Extract unique links
+    gofile_links = list(set(re.findall(r"https?://gofile.io/d/\S+", message.text)))
+    streamtape_links = list(set(re.findall(r"https?://streamtape.to/\S+", message.text)))
 
-    for word in message.text.split():
-        if "gofile.io" in word:
-            gofile_link = word
-        elif "streamtape.to" in word:
-            streamtape_link = word
+    # Prioritize gofile.io links, then streamtape.to links
+    unique_links = gofile_links if gofile_links else streamtape_links
 
-    final_link = gofile_link if gofile_link else streamtape_link
-    if final_link:
-        formatted_text = f"/leech {final_link}\n<b>Tag:</b> <c>@Mr_official_300</c> <c>2142536515</c>"
+    # Send each link one by one
+    for link in unique_links:
+        formatted_text = f"/leech {link}\n<b>Tag:</b> @Mr_official_300 2142536515"
         await client.send_message(DEST_CHANNEL, formatted_text, parse_mode="html")
 
 app.run()
