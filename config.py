7ebbc9f1e4dfbcd509ffb9213d63e12e25fb455e
♕︎ Tʜᴇ Wʜɪᴛᴇ Dᴇᴠɪʟ ♕︎ <99538849+Tahim15@users.noreply.github.com>
2025-03-01 09:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "your_api_id"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "16531092"))
+API_HASH = os.getenv("API_HASH", "b073b97bd4c8c56616fc2cbbd4da845a")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8059489284:AAH2RwyNXy02aWLrAjQ-IvNTDtuF8XQO_RQ")
 SESSION_NAME = "forward_bot"
 
-SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", "-100xxxxxxxxx"))
-DEST_CHANNEL = int(os.getenv("DEST_CHANNEL", "-100xxxxxxxxx"))
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", "-1002398179296"))
+DEST_CHANNEL = int(os.getenv("DEST_CHANNEL", "-1002440398569"))
 TG_WORKERS = 4  # Set to 4 for instant message processing
